3240d4498c5cc09f68c2f12eeb26217e017aec38
etretjakova <etretjakowa@yandex.ru>
2023-05-29 23:54:53 +0700

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Яблоко");
+        fruits.add("Персик");
+        fruits.add("Апельсин");
+
+
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.println(fruits.get(i));
+        }
+        System.out.println();
+
+        Iterator<String>fruitsIterator = fruits.iterator();
+
+        while (fruitsIterator.hasNext()) {
+            System.out.println(fruitsIterator.next());
+        }
+        System.out.println();
+
+        System.out.println("Фрукты:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
     }
 }
